b20f330fb749c8afa7fc6cbe4867ddc0842db3c8
lhllc <contact@leinfelderholt.com>
2025-04-29 12:37:05 -0400

    Create head.tsx

---
+++ app/app/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <meta charSet="utf-8" />
+      <title>vrdAI</title>
+
+      {/* Font Awesome for icon classes like fas fa-robot */}
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+      />
+    </>
+  );
+}
