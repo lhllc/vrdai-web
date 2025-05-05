e25727726cfcbd6a6bffd7b06ff1d2c8ab4ef1c2
Daniel Leinfelder <contact@leinfelderholt.com>
2025-05-05 15:58:57 -0400

    Update Stripe apiVersion to 2025-04-30.basil in config.ts to fix build error

--- utils/stripe/config.ts
+++ utils/stripe/config.ts
@@ -5,7 +5,7 @@ export const stripe = new Stripe(
   {
     // https://github.com/stripe/stripe-node#configuration
     // https://stripe.com/docs/api/versioning
-    apiVersion: '2023-10-16',
+    apiVersion: '2025-04-30.basil',
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
